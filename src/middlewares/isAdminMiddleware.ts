aa9e04aff8ee8bf6aa201a9437e9e0bf9a54ab88
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-09-08 18:52:51 +0300

    Edit docs

--- src/middlewares/isAdminMiddleware.ts
+++ src/middlewares/isAdminMiddleware.ts
@@ -3,7 +3,7 @@ import { CustomError } from "../utils/errorUtils/customError.js";
 
 const isAdmin = (req: Request, res: Response, next: NextFunction) => {
     if (req.user?.role !== "admin") {
-        throw new CustomError("Admin access required", 403);
+        throw new CustomError("Admin access required!", 403);
     }
 
     next();
